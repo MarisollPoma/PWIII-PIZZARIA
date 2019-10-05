0236755a9fcc25a85ad71918182cdbfd5ace794c
Marisol Poma <marisol.poma15@hotmail.com>
2019-10-04 21:26:02 -0300

    Exercicio 7

--- src/app/comanda/comanda.component.ts
+++ src/app/comanda/comanda.component.ts
@@ -11,10 +11,16 @@ export class ComandaComponent implements OnInit {
 
   formPizza : FormGroup;
 
-  constructor (public formBuilder: FormBuilder ) {}
+  constructor (public formBuilder: FormBuilder) {}
 
   ngOnInit() {
-  
+    this.formPizza = this.formBuilder.group({
+      chopp : this.formBuilder.control('Chopp'),
+      pizzas : this.formBuilder.control('Pizzas'),
+      recheios : this.formBuilder.control('Recheios'),
+      pessoas : this.formBuilder.control('Pessoa')
+    })
+    
   }
 
 }
